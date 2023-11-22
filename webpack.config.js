4929764210281c9d3374781b74d043e519234766
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-11-22 05:55:21 +0100

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: ['./src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build/'),
+    filename: 'script.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico',
+    }),
+  ],
+  devServer: {
+    static: false,
+    open: true,
+  },
+};
